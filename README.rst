3775165e6b4dffec186934b5a8cbab00bb1c8cb7
Łukasz Krotowski <lukasz.krotowski@gmail.com>
2015-03-31 03:02:41 +0900

    Initial revision, just license, simple readme and packaging.

---
+++ README.rst
@@ -0,0 +1,31 @@
+Passwdk
+=======
+
+Passwdk is simple console password manager that uses single json file as it's backend.
+
+Usage
+-----
+
+Start passwdk using:
+
+.. code-block:: bash
+
+	$ passwdk --help
+	usage: passwdk [-h]
+
+	optional arguments:
+		-h, --help  show this help message and exit
+
+Requirements
+------------
+
+Passwdk currently is developed and tested on Linux using Python 2.7.
+
+Installation
+------------
+
+The latest version is available to install using `pip <http://www.pip-installer.org/>`_:
+
+.. code-block:: bash
+
+	$ pip install https://github.com/lkrotowski/passwdk/zipball/master
---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from setuptools import setup, find_packages
+
+setup(name         = "passwdk",
+      description  = "Simple console password manager using json as backend",
+      url          = "https://github.com/lkrotowski/passwdk",
+      version      = "0.1.0",
+      packages     = find_packages("src"),
+      package_dir  = {"": "src"},
+      entry_points = {"console_scripts": ["passwdk=passwdk.main:main"]},
+      author       = "Łukasz Krotowski",
+      author_email = "lukasz.krotowski@gmail.com",
+      license      = "GPLv3",
+      classifiers  = ["Development Status :: 3 - Alpha",
+                      "Environment :: Console",
+                      "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+                      "Operating System :: POSIX :: Linux",
+                      "Topic :: Utilities"])
